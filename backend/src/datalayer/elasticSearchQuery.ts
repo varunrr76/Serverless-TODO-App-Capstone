ac3ae64e61d3f951551a1997cde4573bbfc35dce
VarunRaj7 <rvarunraj1@gmail.com>
2020-06-22 21:17:38 -0400

    resolved match issue

--- backend/src/datalayer/elasticSearchQuery.ts
+++ backend/src/datalayer/elasticSearchQuery.ts
@@ -1,112 +1,112 @@
-import * as elasticsearch from 'elasticsearch';
-import * as httpAwsEs from 'http-aws-es';
+import * as elasticsearch from 'elasticsearch'
+import * as httpAwsEs from 'http-aws-es'
 
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
-  APIGatewayProxyResult,
-} from 'aws-lambda';
+  APIGatewayProxyResult
+} from 'aws-lambda'
 
-import { createLogger } from '../utils/logger';
+import { createLogger } from '../utils/logger'
 
-const logger = createLogger('queryTodos');
+const logger = createLogger('queryTodos')
 
-const esHost = process.env.ES_ENDPOINT;
+const esHost = process.env.ES_ENDPOINT
 
 const es = new elasticsearch.Client({
   hosts: [esHost],
-  connectionClass: httpAwsEs,
-});
+  connectionClass: httpAwsEs
+})
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  let queryString;
-  let from;
-  let size;
+  let queryString
+  let from
+  let size
 
   try {
-    queryString = await parseQueryParameter(event);
-    from = await parseFromParameter(event);
-    size = await parseSizeParameter(event);
-    logger.info(`queryString: ${queryString}`);
-    logger.info(`fromParameter: ${from}`);
-    logger.info(`sizeParameter: ${size}`);
+    queryString = await parseQueryParameter(event)
+    from = await parseFromParameter(event)
+    size = await parseSizeParameter(event)
+    logger.info(`queryString: ${queryString}`)
+    logger.info(`fromParameter: ${from}`)
+    logger.info(`sizeParameter: ${size}`)
   } catch (e) {
     return {
       statusCode: 400,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Credentials': true
       },
       body: JSON.stringify({
-        error: 'Invalid Parameters',
-      }),
-    };
+        error: 'Invalid Parameters'
+      })
+    }
   }
   const body = {
     from,
     size,
     query: {
-      wildcard: { name: '*' + queryString + '*' },
-    },
-  };
+      wildcard: { name: +queryString + '*' }
+    }
+  }
   const resp = await es.search({
     index: 'todos-index',
     type: 'todos',
-    body,
-  });
+    body
+  })
   return {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Credentials': true
     },
-    body: JSON.stringify(resp),
-  };
-};
+    body: JSON.stringify(resp)
+  }
+}
 
 async function parseFromParameter(event: APIGatewayProxyEvent) {
-  const fromStr = await getQueryParameter(event, 'from');
+  const fromStr = await getQueryParameter(event, 'from')
   if (!fromStr) {
-    return 0;
+    return 0
   }
 
-  const from = parseInt(fromStr, 10);
+  const from = parseInt(fromStr, 10)
   if (from <= 0) {
-    throw new Error('Limit should be positive');
+    throw new Error('Limit should be positive')
   }
 
-  return from;
+  return from
 }
 
 async function parseSizeParameter(event: APIGatewayProxyEvent) {
-  const sizeStr = await getQueryParameter(event, 'size');
+  const sizeStr = await getQueryParameter(event, 'size')
   if (!sizeStr) {
-    return 10;
+    return 10
   }
 
-  const size = parseInt(sizeStr, 10);
+  const size = parseInt(sizeStr, 10)
   if (size <= 0) {
-    throw new Error('Limit should be positive');
+    throw new Error('Limit should be positive')
   }
 
-  return size;
+  return size
 }
 
 async function parseQueryParameter(event: APIGatewayProxyEvent) {
-  const queryStr = await getQueryParameter(event, 'query');
+  const queryStr = await getQueryParameter(event, 'query')
   if (!queryStr) {
-    return undefined;
+    return undefined
   }
-  return queryStr;
+  return queryStr
 }
 
 async function getQueryParameter(event: APIGatewayProxyEvent, name: string) {
-  const queryParams = event.queryStringParameters;
+  const queryParams = event.queryStringParameters
   if (!queryParams) {
-    return undefined;
+    return undefined
   }
 
-  return queryParams[name];
+  return queryParams[name]
 }
